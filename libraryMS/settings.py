12ab3b68568537da5100b6f0903b330adfbe9f7e
Prem Tamang <57280714+Prem-K-Tamang@users.noreply.github.com>
2020-11-17 19:12:03 +0545

    update setting.py

--- libraryMS/settings.py
+++ libraryMS/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't2n-^9o%jwa)8pnu4!_279%w8(cv(x7-8+9%3n1)@yi8%=x9%@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
